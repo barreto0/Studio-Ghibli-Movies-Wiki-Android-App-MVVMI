3d8dc78ccbf6e29b8b99b9a7158dcdb91205aba4
Gabriel Barreto <antonio909790@yahoo.com.br>
2020-04-09 16:51:23 -0300

    activity login

--- StudioGhibliMovieWiki/app/src/main/java/com/barreto/studio/studioghiblimoviewiki/LoginActivity.kt
+++ StudioGhibliMovieWiki/app/src/main/java/com/barreto/studio/studioghiblimoviewiki/LoginActivity.kt
@@ -3,13 +3,13 @@ package com.barreto.studio.studioghiblimoviewiki
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
         btnLogin.setOnClickListener { login() }
     }
