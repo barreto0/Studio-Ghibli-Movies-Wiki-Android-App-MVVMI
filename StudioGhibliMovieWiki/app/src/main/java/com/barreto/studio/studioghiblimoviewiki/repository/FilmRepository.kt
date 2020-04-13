857b400dfe7b1176e8c337ad5cf51e5626626472
Gabriel Barreto <antonio909790@yahoo.com.br>
2020-04-13 13:58:47 -0300

    layout fix

--- StudioGhibliMovieWiki/app/src/main/java/com/barreto/studio/studioghiblimoviewiki/repository/FilmRepository.kt
+++ StudioGhibliMovieWiki/app/src/main/java/com/barreto/studio/studioghiblimoviewiki/repository/FilmRepository.kt
@@ -23,21 +23,14 @@ interface FilmsService{
 
 }
 
-
-
-
 class FilmsRepository(context: Context, baseUrl: String) : BaseRetrofit(context, baseUrl) {
     private val service = retrofit.create(FilmsService::class.java)
-
     fun getFilmsList(callback: (films: Array<Film>)->Unit){
-
         service.getFilmsList().enqueue(object : Callback<List<FilmDTO>>{
             override fun onResponse(call: Call<List<FilmDTO>>, response: Response<List<FilmDTO>>) {
                 val result = mutableListOf<Film>()
                 val films = response.body()
-
                 films?.forEach { f->
-
                     val domain = Film(
                         id = f.id,
                         title = f.title,
@@ -45,15 +38,11 @@ class FilmsRepository(context: Context, baseUrl: String) : BaseRetrofit(context,
                         director = f.director,
                         releaseDate = f.releaseDate
                     )
-                    Log.d("film","PORRA")
+                    Log.d("film",domain.toString())
                     result.add(domain)
                 }
-
-
-
                 callback(result.toTypedArray())
             }
-
             override fun onFailure(call: Call<List<FilmDTO>>, t: Throwable) {
                 callback(arrayOf()) //array vazio p n quebrar o app
             }
